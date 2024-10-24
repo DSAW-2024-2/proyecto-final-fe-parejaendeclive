87769ecbc897010d1a5bcc385cea5eeb27a632c6
Samuel Rodriguez <samuelroul@unisabana.edu.co>
2024-10-24 18:17:18 -0500

    correcion_2

--- src/Principal.tsx
+++ src/Principal.tsx
@@ -1,3 +1,4 @@
+// App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './principal.css';
@@ -8,10 +9,14 @@ import Pasajero from './pasajeros/pasajeros';
 const Principal: React.FC = () => {
   return (
     <div className="container">
+      {/* Div de fondo */}
+      <div className="background"></div>
+
       <div className="header_principal" style={{ width: '100%', height: '60vh' }}>
         <div className="logo"></div>
         <h1 className="title_principal">Campus Rush</h1>
       </div>
+      
       <div className="button-container_principal">
         <Link to="/login" className="button">Iniciar sesión</Link>
         <Link to="/registro" className="button">Registrarme</Link>
@@ -25,13 +30,9 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Principal />} />
-
         <Route path="/login" element={<Login />} />
-
         <Route path="/registro" element={<Registro />} />
-
         <Route path="/pasajeros" element={<Pasajero />} />
-
         <Route path="/Principal" element={<Principal />} />
       </Routes>
     </Router>
